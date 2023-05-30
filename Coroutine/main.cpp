1c54d20030249b74abe79340216cdb3e20c8ca35
luoqiangwei <luoqiangwei@live.cn>
2023-05-30 08:00:25 +0800

    第一次简单的协程实现

--- Coroutine/main.cpp
+++ Coroutine/main.cpp
@@ -6,9 +6,50 @@
 //
 
 #include <iostream>
+#include <coroutine>
+
+using namespace std;
+
+struct CoRet {
+    struct promise_type {
+        int _in;
+        suspend_never initial_suspend() { return {}; }
+        suspend_never final_suspend() noexcept { return {}; }
+        void unhandled_exception() {}
+        CoRet get_return_object() {
+            return  {
+                coroutine_handle<promise_type>::from_promise(*this)
+            };
+        }
+    };
+    
+    coroutine_handle<promise_type> _h; // _h.resume() = _h()
+};
+
+struct Note {
+    int guess;
+};
+
+struct Input {
+    int* _in;
+    bool await_ready() { return false; }
+    void await_suspend(coroutine_handle<CoRet::promise_type> h) {
+//        h.promise()
+        _in = &h.promise()._in;
+    }
+    int await_resume() { return *_in; }
+};
+
+CoRet Guess() {
+    Input input;
+    int g = co_await input;
+    cout << "coroutine: You guess " << g << endl;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    auto ret = Guess();
+    cout << "main: make a guess ..." << endl;
+    ret._h.promise()._in = 100;
+    ret._h.resume();
     return 0;
 }
